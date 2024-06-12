1b67ca4e301607a62bd0ce853c6788906f0a632f
TiboMertens <tibomertens25@gmail.com>
2024-06-12 12:19:50 +0200

    fix: background position iphone

--- js/check-iphone.js
+++ js/check-iphone.js
@@ -5,4 +5,18 @@ function isIPhone() {
 if (isIPhone()) {
   document.querySelector(".btn-container").style.display = "none";
   document.querySelector(".app-store-container").style.display = "block";
+  // select s-hero::before
+  const styleSheet = document.styleSheets[0];
+
+  // Find the rule you want to modify
+  for (let i = 0; i < styleSheet.cssRules.length; i++) {
+    const rule = styleSheet.cssRules[i];
+
+    if (rule.selectorText === ".s-hero::before") {
+      // Modify the height and top properties
+      rule.style.height = "30%";
+      rule.style.top = "70%";
+      break;
+    }
+  }
 }
