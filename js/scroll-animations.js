0eb63e6120d45a50b3ea0488495d27bee61fd1cb
TiboMertens <tibomertens25@gmail.com>
2024-06-15 15:02:54 +0200

    feat: add scroll animations

---
+++ js/scroll-animations.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 1500, // duration of the animation
+    once: false, // whether animation should happen only once - while scrolling down
+  });
+});
